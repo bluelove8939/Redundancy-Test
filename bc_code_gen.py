c99bfb6096f66d1f954f603892783f0982ee7096
bluelove8939 <bluelove8939@naver.com>
2022-11-07 15:34:16 +0900

    Initial commit

--- bc_code_gen.py
+++ bc_code_gen.py
@@ -1,7 +1,5 @@
 import os
-import subprocess
 import math
-import inspect
 
 from utils.verilog_generator import VerilogGenerator
 
--- utils/verilog_generator.py
+++ utils/verilog_generator.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import math
 import inspect
 
 
